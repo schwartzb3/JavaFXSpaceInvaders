5d3d54399235da2f8bc36a83e88b4a8801c7e3f6
schwartzb <schwartzb@carleton.edu>
2019-05-30 19:24:09 -0500

    added some window stuff and peter changed button stuff

--- src/ButtonSample.java
+++ src/ButtonSample.java
@@ -1,119 +1,104 @@
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
+import javafx.scene.layout.Pane;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-/**
- * A slightly more complex GUI, with buttons and text and multiple layouts.
- * Created by adalal on 2/23/16.
- */
+
 public class ButtonSample extends Application {
-    public static final double MIN_BUTTON_WIDTH = 60;
-    private Text directions = new Text("Don't Press The Left Buttons");
-    private Text feedback = new Text("");
-    private int ohNo;
+    private Pane root = new Pane();
+
+    private Sprite player = new Sprite(200, 750, 30, 30, "player", Color.RED);
+
+    private Parent createContent() {
+        root.setPrefSize(600, 800);
+        root.getChildren().add(player);
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                update();
+            }
+        };
+        timer.start();
+
+        nextLevel();
+
+        return root;
+    }
+
+    private void nextLevel() {
+        for (int i = 0; i < 5; i++) {
+            Sprite s = new Sprite(90 + i * 100, 150, 30, 30, "enemy", Color.GREEN);
+            root.getChildren().add(s);
+        }
+    }
+
+    private void update() {
+
+    }
+
+
+    private void shoot() {
+
+    }
 
     @Override
-    public void start(Stage primaryStage) {
-        // BORDER PANE DIVIDES THE THING INTO DIFFERENT AREAS
-        BorderPane root = new BorderPane();
-        Node buttonPane = addButtons();
-        Node directionsPane = addText(directions, FontWeight.BOLD, 20);
-        Node feedbackPane = addText(feedback, FontWeight.NORMAL, 14);
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(createContent());
 
-        // BORDER PANE SETTING TOP AND CENTER AND BOTTOM
-        root.setTop(directionsPane);
-        root.setCenter(buttonPane);
-        root.setBottom(feedbackPane);
+        scene.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case A:
+                    player.moveLeft();
+                    break;
+                case D:
+                    player.moveRight();
+                    break;
+                case SPACE:
+                    shoot();
+                    break;
 
-        Scene scene = new Scene(root, 300, 250);
-        primaryStage.setTitle("Buttons, buttons everywhere");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            }
+        });
+
+        stage.setScene(scene);
+        stage.show();
     }
 
+    private static class Sprite extends Rectangle {
+        boolean dead = false;
+        final String type;
 
-    private Node addButtons() {
-        ohNo = 0;
-        GridPane buttonPane = new GridPane();
-        buttonPane.setAlignment(Pos.CENTER);
-        buttonPane.setHgap(5);
-        buttonPane.setVgap(5);
-        buttonPane.setPadding(new Insets(0, 10, 0, 10));
+        Sprite(int x, int y, int w, int h, String type, Color color) {
+            super(w, h, color);
 
-        // Create the four buttons and add them to the grid
-        Button eeny = new Button("eeny");
-        eeny.setMinWidth(MIN_BUTTON_WIDTH);
-        buttonPane.add(eeny, 0, 0);
-        Button meeny = new Button("meeny");
-        meeny.setMinWidth(MIN_BUTTON_WIDTH);
-        buttonPane.add(meeny, 1, 0);
-        Button miney = new Button("miney");
-        miney.setMinWidth(MIN_BUTTON_WIDTH);
-        buttonPane.add(miney, 0, 1);
-        Button moe = new Button("moe");
-        moe.setMinWidth(MIN_BUTTON_WIDTH);
-        buttonPane.add(moe, 1, 1);
+            this.type = type;
+            setTranslateX(x);
+            setTranslateY(y);
+        }
 
-        // Associate actions with each button
-        eeny.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                if (ohNo % 2 == 0) {
-                    feedback.setText("ASDJKNSKJADNKJ");
-                    ohNo++;
-                } else {
-                    feedback.setText("Pressed " + meeny.getText());
-                    ohNo++;
-                }
-            }
-        });
+        void moveLeft() {
+            setTranslateX(getTranslateX() - 5);
+        }
 
-        meeny.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                feedback.setText("Pressed " + meeny.getText());
-            }
-        });
+        void moveRight() {
+            setTranslateX(getTranslateX() + 5);
+        }
 
-        miney.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                feedback.setText("NONONONONONONONONO NOT THAT FORREST");
-            }
-        });
+        void moveUp() {
+            setTranslateY(getTranslateY() - 5);
+        }
 
-        moe.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                feedback.setText("Pressed " + moe.getText());
-            }
-        });
-
-        return buttonPane;
-    }
-
-    private Node addText(Text text, FontWeight fontWeight, int fontSize) {
-        FlowPane flowPane = new FlowPane();
-        flowPane.setAlignment(Pos.CENTER);
-        text.setFont(Font.font("Times New Roman", fontWeight, fontSize));
-        flowPane.getChildren().add(text);
-        return flowPane;
+        void moveDown() {
+            setTranslateY(getTranslateY() + 5);
+        }
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 }
